32ee303c1a958ec80dfae833c506ff9ed5f6b8dd
ygxdxx <aaa100007673@163.com>
2018-10-15 21:15:25 +0800

    添加schema

--- node/loc8r/app.js
+++ node/loc8r/app.js
@@ -1,8 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./app_server/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
