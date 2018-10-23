579c0669de435224c512483f6ad6f8d1eb96ed2e
ygxdxx <aaa100007673@163.com>
2018-10-23 20:16:01 +0800

    切换express的router到angular的router

--- node/loc8r/app.js
+++ node/loc8r/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
 //两套路由系统 app_server & api
 app.use('/', routes);
--- node/loc8r/app_server/routes/index.js
+++ node/loc8r/app_server/routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var locations = require('../controllers/locations');
 var others = require('../controllers/others');
 
-//location page
-router.get('/', locations.homepage);
-router.get('/location/:locationid', locations.detailpage);
-router.get('/location/:locationid/reviews/new', locations.reviewpage); //获取填写评论的页面
-router.post('/location/:locationid/reviews/new', locations.postreview); //像服务器发送平路内容
+router.get('/',others.angularApp);
 
-//other page
+// var locations = require('../controllers/locations');
+// router.get('/', locations.homepage);
+// router.get('/location/:locationid', locations.detailpage);
+// router.get('/location/:locationid/reviews/new', locations.reviewpage); //获取填写评论的页面
+// router.post('/location/:locationid/reviews/new', locations.postreview); //像服务器发送平路内容
+
 router.get('/about', others.about);
 
 module.exports = router;
