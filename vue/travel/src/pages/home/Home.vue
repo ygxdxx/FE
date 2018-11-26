61fdb3d07a4e9072698e63d9804a482210c4f5d9
ygxdxx <aaa100007673@163.com>
2018-11-26 14:05:39 +0800

    change

--- vue/travel/src/pages/home/Home.vue
+++ vue/travel/src/pages/home/Home.vue
@@ -2,16 +2,19 @@
   <!--template只能存入一个标签,因此使用div进行包裹-->
   <div>
     <home-header/>
+    <home-swiper/>
   </div>
 </template>
 
 <script>
   import HomeHeader from './components/Header'
+  import HomeSwiper from './components/Swiper'
 
   export default {
     name: 'Home',
     components: {
-      HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
   }
 </script>
---
+++ vue/travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide
+        v-for="item of swiperList"
+        :key="item.id">
+        <img
+          :src="item.imgUrl"
+          class="swiper-img">
+      </swiper-slide>
+      <div
+        slot="pagination"
+        class="swiper-pagination"
+      />
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeSwiper',
+    data() {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        swiperList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/4d/6706bf772c05ce02.jpg_750x200_773e75f0.jpg'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/31/da037478f37cf202.jpg_750x200_fe28d396.jpg'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swiper-img
+      width: 100%
+      background: #eee
+</style>
