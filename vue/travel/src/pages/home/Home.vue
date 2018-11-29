04da40ed0d48cc7c8df5a1ff448c15594a257347
ygxdxx <aaa100007673@163.com>
2018-11-29 21:48:58 +0800

    keep alive finish

--- vue/travel/src/pages/home/Home.vue
+++ vue/travel/src/pages/home/Home.vue
@@ -15,6 +15,7 @@
   import HomeRecommend from './components/Recommend'
   import HomeWeekend from './components/Weekend'
   import axios from 'axios'
+  import {mapState} from 'vuex'
 
   export default {
     name: 'Home',
@@ -27,15 +28,19 @@
     },
     data() {
       return {
+        lastCity: '',
         swiperList: [],
         iconList: [],
         recommendList: [],
         weekendList: []
       }
     },
+    computed: {
+      ...mapState(['city'])
+    },
     methods: {
       getHomeInfo() {
-        axios.get('/api/index.json').then(this.getHomeInfoSucc)
+        axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
       },
       getHomeInfoSucc(res) {
         res = res.data
@@ -50,6 +55,13 @@
     },
     mounted() {
       this.getHomeInfo()
+      this.lastCity = this.city
+    },
+    activated() {
+      if (this.lastCity !== this.city) {
+        this.lastCity = this.city
+        this.getHomeInfo()
+      }
     }
   }
 </script>
